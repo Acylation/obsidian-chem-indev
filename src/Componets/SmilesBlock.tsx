50ca934f62431a49609535c96ebe91ba5a67e28b
Acylation <532117255@qq.com>
2023-04-01 13:43:52 +0800

    remove gpl licence

--- src/Componets/SmilesBlock.tsx
+++ src/Componets/SmilesBlock.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SmilesDrawer from "smiles-drawer";
-import {
-  MarkdownPostProcessor,
-  MarkdownPostProcessorContext,
-  MarkdownPreviewRenderer,
-  MarkdownRenderChild,
-  MarkdownRenderer,
-  MarkdownView,
-} from "obsidian";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import SmilesDrawer from "smiles-drawer";
+// import {
+//   MarkdownPostProcessor,
+//   MarkdownPostProcessorContext,
+//   MarkdownPreviewRenderer,
+//   MarkdownRenderChild,
+//   MarkdownRenderer,
+//   MarkdownView,
+// } from "obsidian";
 
 
-class SmilesBlock extends React.Component {
-  constructor(props){
-    super(props);
-  }
-  render() {
-  }
-}
+// class SmilesBlock extends React.Component {
+//   constructor(props){
+//     super(props);
+//   }
+//   render() {
+//   }
+// }
 
-const root = ReactDOM.createRoot(this.containerEl.children[1]);
-root.render(<table />);
+// const root = ReactDOM.createRoot(this.containerEl.children[1]);
+// root.render(<table />);
 
-export const ReactView = () => {
-  return <h4>Hello, React!</h4>;
-};
+// export const ReactView = () => {
+//   return <h4>Hello, React!</h4>;
+// };
 
-//hover event
-//button: copy
-//click event
+// //hover event
+// //button: copy
+// //click event
